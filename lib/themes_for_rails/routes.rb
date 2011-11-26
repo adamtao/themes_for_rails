e615c11c6e6c9f4d9297cdf6d58bef0479d0cba8
Adam Anderson <adam@makeascene.com>
2011-11-25 23:18:16 -0700

    Trying to fix asset routes for rails 3.1.1

--- lib/themes_for_rails/routes.rb
+++ lib/themes_for_rails/routes.rb
@@ -2,9 +2,9 @@ module ThemesForRails
   module Routes
     def themes_for_rails
       theme_dir = ThemesForRails.config.themes_dir
-      match "#{theme_dir}/:theme/stylesheets/*asset" => 'themes_for_rails/assets#stylesheets', :as => :base_theme_stylesheet
-      match "#{theme_dir}/:theme/javascripts/*asset" => 'themes_for_rails/assets#javascripts', :as => :base_theme_javascript
-      match "#{theme_dir}/:theme/images/*asset" => 'themes_for_rails/assets#images', :as => :base_theme_image
+      match "#{theme_dir}/:theme/stylesheets/:asset" => 'themes_for_rails/assets#stylesheets', :as => :base_theme_stylesheet, :constraints => { :asset => /.*/ }
+      match "#{theme_dir}/:theme/javascripts/:asset" => 'themes_for_rails/assets#javascripts', :as => :base_theme_javascript, :constraints => { :asset => /.*/ }
+      match "#{theme_dir}/:theme/images/:asset" => 'themes_for_rails/assets#images', :as => :base_theme_image, :constraints => { :asset => /.*/ }
     end
   end
 end
